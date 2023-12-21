3f301c33a159c3932065df0610d60cc75129c657
Djavan Gabriel <=>
2023-12-20 21:39:28 -0300

    change s navegation

--- resources/views/Site/layout.blade.php
+++ resources/views/Site/layout.blade.php
@@ -929,6 +929,7 @@
 </html>
 
 @section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
         import './bootstrap';
 
